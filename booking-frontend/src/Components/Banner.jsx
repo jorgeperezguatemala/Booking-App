252448b8794e8f4922b24009fcab8f1e2e1fd14e
Jonatan Flores <jonatandavidfloresperez@gmail.com>
2023-09-04 17:52:23 -0600

    :wrench:: feat (add): add componets

---
+++ booking-frontend/src/Components/Banner.jsx
@@ -0,0 +1,11 @@
+const Banner = () => {
+    return (
+        <>
+            <div class="container">
+            <img className='relative w-full' src="../Resources/Banner/IMG_8690.jpg" alt="Surf" />
+            </div>
+        </>
+    )
+  }
+  
+  export default Banner
---
+++ booking-frontend/src/Pages/HomePage.jsx
@@ -0,0 +1,15 @@
+import Banner from "../Components/Banner"
+import NavBar from "../Components/NavBar"
+
+const HomePage = () => {
+  return (
+    <>
+    <NavBar />
+    <Banner />
+
+
+    </>
+  )
+}
+
+export default HomePage
