591732917f4962dc7bc7ad450ceae688f4d009b1
GOTLERH <gersonmiranda94@yahoo.es>
2023-09-08 16:48:02 -0600

    My-Bookings backend integration and footer.

--- booking-frontend/src/App.jsx
+++ booking-frontend/src/App.jsx
@@ -1,32 +1,29 @@
-import { useState } from 'react'
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
-import './App.css'
-import HomePage from './Pages/HomePage'
-import BookingPage from './Pages/BookingPage'
-import MyBookings from './Pages/MyBookings'
-import BookNow from './Pages/BookNow'
-import AboutUsPage from './Pages/AboutUsPage'
-import SurfTripsPages from './Pages/SurfTripsPages'
-import BookNowConfirm from './Pages/BookNowConfirm'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import HomePage from "./Pages/HomePage";
+import BookingPage from "./Pages/BookingPage";
+import MyBookings from "./Pages/MyBookings";
+import BookNow from "./Pages/BookNow";
+import AboutUsPage from "./Pages/AboutUsPage";
+import SurfTripsPages from "./Pages/SurfTripsPages";
+import BookNowConfirm from "./Pages/BookNowConfirm";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-        <BrowserRouter>
+      <BrowserRouter>
         <Routes>
-          <Route path='/' element={<HomePage/>}></Route>
-          <Route path='/booking' element={<BookingPage />}></Route>
-          <Route path='/mybookings' element={<MyBookings />}></Route>
-          <Route path='/booknow' element={<BookNow/>}></Route>
-          <Route path='/aboutus' element={<AboutUsPage/>}></Route>
-          <Route path='/surftrips' element={<SurfTripsPages/>}></Route>
-          <Route path='/booknowconfirm' element={<BookNowConfirm/>}></Route>
+          <Route path="/" element={<HomePage />}></Route>
+          <Route path="/booking" element={<BookingPage />}></Route>
+          <Route path="/mybookings" element={<MyBookings />}></Route>
+          <Route path="/booknow" element={<BookNow />}></Route>
+          <Route path="/aboutus" element={<AboutUsPage />}></Route>
+          <Route path="/surftrips" element={<SurfTripsPages />}></Route>
+          <Route path="/booknowconfirm" element={<BookNowConfirm />}></Route>
         </Routes>
       </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
